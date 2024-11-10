e99a34ea588e0349150020e463fbbabb54e9ac9e
soma <somofgit@gmail.com>
2024-11-10 14:13:58 +0900

    次の目標（引数の削除）

--- test/wayaku_test.rb
+++ test/wayaku_test.rb
@@ -20,62 +20,63 @@ class User < ActiveRecord::Base
   enumerize :status, in: %i[sleeping working mystery]
 end
 
+# TODO Improve readability
 class WayakuTest < Minitest::Test
   def test_wayaku
-    assert_output(<<~TEXT) { User.wayaku(bool: false) }
-      ユーザー
-      user
-          ID
-          id
-          名前
-          name
-          ステータス
-          status
-              寝ている
-              sleeping
-              働いている
-              working
-              謎に包まれている
-              mystery
+    assert_output(<<~TEXT) { User.wayaku }
+      \e[38;5;255mユーザー\e[0m
+      \e[38;5;255muser\e[0m
+          \e[38;5;2mID\e[0m
+          \e[38;5;2mid\e[0m
+          \e[38;5;2m名前\e[0m
+          \e[38;5;2mname\e[0m
+          \e[38;5;2mステータス\e[0m
+          \e[38;5;2mstatus\e[0m
+              \e[38;5;3m寝ている\e[0m
+              \e[38;5;3msleeping\e[0m
+              \e[38;5;3m働いている\e[0m
+              \e[38;5;3mworking\e[0m
+              \e[38;5;3m謎に包まれている\e[0m
+              \e[38;5;3mmystery\e[0m
     TEXT
   end
 
   def test_wayaku_enum_with_right_argment
-    assert_output(<<~TEXT) { User.wayaku_enum(:status, bool: false) }
-      ステータス
-      status
-          寝ている
-          sleeping
-          働いている
-          working
-          謎に包まれている
-          mystery
+    assert_output(<<~TEXT) { User.wayaku_enum(:status) }
+      \e[38;5;2mステータス\e[0m
+      \e[38;5;2mstatus\e[0m
+          \e[38;5;3m寝ている\e[0m
+          \e[38;5;3msleeping\e[0m
+          \e[38;5;3m働いている\e[0m
+          \e[38;5;3mworking\e[0m
+          \e[38;5;3m謎に包まれている\e[0m
+          \e[38;5;3mmystery\e[0m
     TEXT
   end
 
   def test_wayaku_enum_with_wrong_argment
-    assert_output("\e[38;5;196m知らない属性\e[0m\n") { User.wayaku_enum(:hoge, bool: false) }
+    assert_output("\e[38;5;196m知らない属性\e[0m\n") { User.wayaku_enum(:hoge) }
   end
 
   def test_wayaku_logicals
     assert_output(<<~TEXT) { User.wayaku_logicals }
-      ID
-      名前
-      ステータス
-          寝ている
-          働いている
-          謎に包まれている
+      \e[38;5;2mID\e[0m
+      \e[38;5;2m名前\e[0m
+      \e[38;5;2mステータス\e[0m
+          \e[38;5;3m寝ている\e[0m
+          \e[38;5;3m働いている\e[0m
+          \e[38;5;3m謎に包まれている\e[0m
     TEXT
   end
 
   def test_wayaku_physicals
     assert_output(<<~TEXT) { User.wayaku_physicals }
-      id
-      name
-      status
-          sleeping
-          working
-          mystery
+      \e[38;5;2mid\e[0m
+      \e[38;5;2mname\e[0m
+      \e[38;5;2mstatus\e[0m
+          \e[38;5;3msleeping\e[0m
+          \e[38;5;3mworking\e[0m
+          \e[38;5;3mmystery\e[0m
     TEXT
   end
 end
